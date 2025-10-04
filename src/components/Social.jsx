70b26027721747d5abc4bff0e659941409aaccb1
Viktoriya Hristova <vikihris03@gmail.com>
2025-10-04 21:46:48 +0100

    added links

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,11 +2,21 @@ import GitHubLogo from "../assets/logos/github.svg"
 import LinkedInLogo from "../assets/logos/linkedin.svg"
 import Box from "./Box.jsx"
 
+const links = {
+  github: "https://github.com/vikihh/",
+  linkedin: "https://www.linkedin.com/in/viktoriya-hristova-963946273/"
+};
+
 function Social({ logo = 'github' }) {
   return (
     <Box content=
-      {<img src={(logo == 'github') ? LinkedInLogo : GitHubLogo} alt="Logo" width="50" height="50" />}
-      className={"button"} />
+      {
+        <a href={links[logo]} target="_blank">
+          <img src={(logo == 'linkedin') ? LinkedInLogo : GitHubLogo} alt="Logo" width="50" height="50" />
+        </a>
+      }
+      className="button"
+    />
   );
 }
 
