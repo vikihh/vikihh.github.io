05782165b4d698cc5d9324c7261d1aae61f4b232
Viktoriya Hristova <vikihris03@gmail.com>
2025-10-06 00:41:56 +0100

    added a background picture

--- src/App.jsx
+++ src/App.jsx
@@ -9,33 +9,37 @@ import Theme from "./components/Theme.jsx"
 import profile_photo from "./assets/photos/profile_photo.jpg"
 import radcam from "./assets/photos/radcliffe_camera.jpg"
 import softwire from "./assets/photos/softwire_logo.jpg"
+import background from "./assets/background.svg"
 import texts from "./assets/texts.json"
 
 function App() {
   return (
-    <div className='vertical-container'>
-      <Title />
-      <div className='horizontal-container'>
-        <div className='vertical-container'>
-          <Picture photo={profile_photo} />
-          <div className='horizontal-small-gap-container'>
-            <Theme />
-            <Social logo={'github'} />
-            <Social logo={'linkedin'} />
+    <div className='page'>
+      <img src={background} alt="" className="background" />
+      <div className='vertical-container'>
+        <Title />
+        <div className='horizontal-container'>
+          <div className='vertical-container'>
+            <Picture photo={profile_photo} />
+            <div className='horizontal-small-gap-container'>
+              <Theme />
+              <Social logo={'github'} />
+              <Social logo={'linkedin'} />
+            </div>
           </div>
+          <Text content={'intro'} />
         </div>
-        <Text content={'intro'} />
-      </div>
-      <div className='horizontal-container'>
-        <Skills />
-      </div>
-      <div className='horizontal-container'>
-        <Text content={'education'} />
-        <Picture photo={radcam} />
-      </div>
-      <div className='horizontal-container'>
-        <Picture photo={softwire} />
-        <Text content={'internship'} />
+        <div className='horizontal-container'>
+          <Skills />
+        </div>
+        <div className='horizontal-container'>
+          <Text content={'education'} />
+          <Picture photo={radcam} />
+        </div>
+        <div className='horizontal-container'>
+          <Picture photo={softwire} />
+          <Text content={'internship'} />
+        </div>
       </div>
     </div>
   )
