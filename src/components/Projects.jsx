1d3a1e352b5aa6a412becb3a532d5cf6adeff579
Viktoriya Hristova <vikihris03@gmail.com>
2025-11-12 14:45:21 +0000

    projects: add left and right buttons in section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,28 @@
 import Box from "./Box.jsx"
+import LeftArrow from "../assets/icons/left.png"
+import RightArrow from "../assets/icons/right.png"
 
 function Projects() {
   return (
     <section id="projects">
-      <Box content={
-        <div>
-          Project 1
+      <div className="horizontal-container">
+        <Box content={""}
+          className="stretched" />
+        <div className="horizontal-small-gap-container">
+          <Box content=
+            {
+              <img src={LeftArrow} alt="Left" style={{ width: "3em", height: "auto" }} />
+            }
+            className="button"
+          />
+          <Box content=
+            {
+              <img src={RightArrow} alt="Right" style={{ width: "3em", height: "auto" }} />
+            }
+            className="button"
+          />
         </div>
-      } className="stretched name-of-project" />
+      </div>
     </section>
   );
 }
