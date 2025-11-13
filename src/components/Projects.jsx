46cec124a0824c56005ad5a969b58f8a4c362167
Viktoriya Hristova <vikihris03@gmail.com>
2025-11-13 17:32:17 +0000

    projects: display and change project description

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,27 +1,38 @@
 import Box from "./Box.jsx"
+import Text from './Text.jsx';
 import LeftArrow from "../assets/icons/left.png"
 import RightArrow from "../assets/icons/right.png"
+import texts from "../assets/texts.json"
+import { useState } from "react";
 
 function Projects() {
+  const cards = texts.projects;
+  const [index, setIndex] = useState(0);
+  const next = () => setIndex((i) => (i + 1) % cards.length);
+  const prev = () => setIndex((i) => (i - 1 + cards.length) % cards.length);
+
   return (
     <section id="projects">
       <div className="horizontal-container">
-        <Box content={""}
-          className="stretched" />
         <div className="horizontal-small-gap-container">
           <Box content=
             {
-              <img src={LeftArrow} alt="Left" style={{ width: "3em", height: "auto" }} />
+              <div className="arrow" onClick={prev}>
+                <img src={LeftArrow} alt="Left" style={{ width: "2em", height: "2em", display: "flex" }} />
+              </div>
             }
             className="button"
           />
           <Box content=
             {
-              <img src={RightArrow} alt="Right" style={{ width: "3em", height: "auto" }} />
+              <div className="arrow" onClick={next}>
+                <img src={RightArrow} alt="Right" style={{ width: "2em", height: "2em", display: "flex" }} />
+              </div>
             }
             className="button"
           />
         </div>
+        <Text children={cards[index]} />
       </div>
     </section>
   );
