a7cfb65ebb88b51000003c233e8f1a85dcf9fb42
Viktoriya Hristova <vikihris03@gmail.com>
2025-11-14 16:32:44 +0000

    change text rendering

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,24 +1,28 @@
 import Box from "./Box.jsx"
 import texts from "../assets/texts.json"
 
+function renderTextWithHighlights(segment, i) {
+    return segment.text.split('\n').flatMap((line, j) => [
+        <span key={`${i}-${j}`} className={segment.highlight ? 'highlight' : ''}>
+            {line}
+        </span>,
+        j < segment.text.split('\n').length - 1 && <br key={`br-${i}-${j}`} />
+    ]);
+}
+
 function Text({ children }) {
     const key = typeof children === "string" ? children : null;
     const content = texts[key] ? texts[key] : children;
     if (Array.isArray(content)) {
-    return (
-        <Box content={
-            <p>
-                {content.map((segment, i) =>
-                    segment.highlight ? (
-                        <span key={i} className="highlight">{segment.text}</span>
-                    ) : (
-                        <span key={i}>{segment.text}</span>
-                    )
-                )}
-            </p>
-        }
-        className={"stretched"} />
-    );}
+        return (
+            <Box content={
+                <p>
+                    {content.flatMap((segment, i) => renderTextWithHighlights(segment, i))}
+                </p>
+            }
+            className={"stretched"} />
+        );
+    }
     return (
         <Box
             content={content}
