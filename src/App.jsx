720408f0cc00e1baa29c1c3b6d57b73879ae60b5
Viktoriya Hristova <vikihris03@gmail.com>
2025-11-14 18:06:40 +0000

    add router navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,16 @@
-import './App.css'
-import Social from './components/Social.jsx'
-import Title from './components/Title.jsx'
-import Text from './components/Text.jsx'
-import Picture from './components/Picture.jsx'
-import Skills from './components/Skills.jsx'
-import Theme from "./components/Theme.jsx"
-import profile_photo from "./assets/photos/profile_photo.jpg"
-import radcam from "./assets/photos/radcliffe_camera.jpg"
-import softwire from "./assets/photos/softwire_logo.png"
-import background from "./assets/background.svg"
-import Box from './components/Box.jsx'
-import Projects from './components/Projects.jsx'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Game from './pages/Game';
 
 function App() {
   return (
-    <div className='page'>
-      <img src={background} alt="" className="background" />
-      <div className='vertical-container'>
-        <Title />
-        <div className='horizontal-container'>
-          <div className='vertical-container'>
-            <Picture photo={profile_photo} />
-            <div className='horizontal-small-gap-container'>
-              <Theme />
-              <Social logo={'github'} />
-              <Social logo={'linkedin'} />
-            </div>
-            <Box content= {<a href="#projects">Projects</a>} className="button"/>
-          </div>
-          <Text children={'intro'} />
-        </div>
-        <div className='horizontal-container'>
-          <Skills />
-        </div>
-        <div className='horizontal-container'>
-          <Text children={'education'} />
-          <Picture photo={radcam} />
-        </div>
-        <div className='horizontal-container'>
-          <Picture photo={softwire} />
-          <Text children={'internship'} />
-        </div>
-        <Projects />
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/game" element={<Game/>}/>
+      </Routes>
+    </Router>
+
   )
 }
 
---
+++ src/pages/Game.jsx
@@ -0,0 +1,15 @@
+import '../App.css'
+import background from "../assets/background.svg"
+
+function Game() {
+  return (
+    <div className='page'>
+      <img src={background} alt="" className="background" />
+      <div>
+        <p>Coming soon...</p>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
