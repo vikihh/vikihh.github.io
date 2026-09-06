36fc823ee2898011232b1814f11d5c743a481be6
Viktoriya Hristova <vikihris03@gmail.com>
2026-09-06 12:12:15 +0100

    adding initial foldable toolbar

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -3,20 +3,31 @@ import '../App.css'
 import Social from '../components/Social.jsx'
 import '../styles/Toolbar.css'
 import { FaDice } from "react-icons/fa";
+import { useState } from 'react';
 
 function Toolbar() {
+  const [expanded, setExpanded] = useState(true);
   return (
-    <div className="toolbar">
-      <Theme />
-      <Social logo={'github'} />
-      <Social logo={'linkedin'} />
-      <div className={"box social-button"}>
-      {
-        <a href="#/games">
-          <FaDice size={"2em"}/>
-        </a>
-      }
-    </div>
+    <div className={`toolbar ${expanded ? "expanded" : "folded"}`}>
+      <button
+        className="toolbar-toggle"
+        onClick={() => setExpanded(!expanded)}
+      >
+        ☰
+      </button>
+      {expanded && (
+        <div className="toolbar-content">
+          <Theme />
+          <Social logo={'github'} />
+          <Social logo={'linkedin'} />
+          <div className={"box social-button"}>
+          {
+            <a href="#/games">
+            <FaDice size={"2em"}/>
+            </a>
+          }
+          </div>
+        </div>)}
     </div>
   );
 }
